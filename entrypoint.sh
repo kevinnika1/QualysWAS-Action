ec3573c14a9485dde4fa00d255d92962c24ceacc
kevinnika1 <87488963+kevinnika1@users.noreply.github.com>
2021-09-24 15:14:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
-python qualysdast-api.py --qualysuser $1 --qualyspass $2 --website $3 --project $4
+echo 'hello'
+# python qualysdast-api.py --qualysuser $1 --qualyspass $2 --website $3 --project $4
